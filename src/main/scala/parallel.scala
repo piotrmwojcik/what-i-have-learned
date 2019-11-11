fc5908001a6b80460a3bfb00056b3d1de93304b8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2019-11-11 23:08:38 +0100

    parallel updated

---
+++ src/main/scala/parallel.scala
@@ -0,0 +1,35 @@
+import java.time.Instant
+import java.util.concurrent.{Callable, ExecutorService, Future}
+
+import scala.concurrent.duration.TimeUnit
+
+type Par[A] = ExecutorService => Future[A]
+
+object Par {
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, unit: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get, bf.get))
+  }
+
+  def fork[A](a: => Par[A]): Par[A] = (es: ExecutorService) => es.submit(new Callable[A] {
+    def call: A = a(es).get
+  })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+}
+
+object test extends App {
+
+}
