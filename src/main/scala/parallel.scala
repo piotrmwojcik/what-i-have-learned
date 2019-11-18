743010af560ec69d59a4b69e74507c97c6b2ab79
Piotr Wojcik <piotr.wojcik@ls2.merkursoft.local>
2019-11-18 14:58:38 +0100

    parallel updated

--- src/main/scala/parallel.scala
+++ src/main/scala/parallel.scala
@@ -1,24 +1,44 @@
 import java.time.Instant
-import java.util.concurrent.{Callable, ExecutorService, Future}
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
 import scala.concurrent.duration.TimeUnit
 
-type Par[A] = ExecutorService => Future[A]
+
 
 object Par {
-  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
+
     def get(timeout: Long, unit: TimeUnit) = get
+
     def isCancelled = false
+
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
-    val af = a(es)
-    val bf = b(es)
-    UnitFuture(f(af.get, bf.get))
+    new Future[C] {
+      def isDone = isDone
+
+      def get = get()
+
+      def get(timeout: Long, unit: TimeUnit) = {
+        val ts = Instant.now.toEpochMilli
+        val af = a(es).get(timeout, unit)
+        val duration = unit.convert(Instant.now.toEpochMilli - ts, unit)
+        val bf = b(es).get(timeout - duration, unit)
+        f(af, bf)
+      }
+
+      def isCancelled: Boolean = isCancelled
+
+      def cancel(evenIfRunning: Boolean): Boolean = cancel(evenIfRunning)
+    }
   }
 
   def fork[A](a: => Par[A]): Par[A] = (es: ExecutorService) => es.submit(new Callable[A] {
@@ -30,6 +50,9 @@ object Par {
   def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
 }
 
-object test extends App {
-
+object testApp1 extends App {
+  val executorService = java.util.concurrent.Executors.newFixedThreadPool(5)
+  val a = Par.lazyUnit({println("starting 1st"); Thread.sleep(4600); println("A"); "a"})
+  val b = Par.lazyUnit({println("starting 2nd"); Thread.sleep(4600); println("B"); "b"})
+  println(Par.map2(a, b)(Seq(_, _).mkString)(executorService).get(9400, TimeUnit.MILLISECONDS))
 }
